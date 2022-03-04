6ad183b4461c3281668ff5a0fa7d81b830e1a3c0
kvyatkovskys <sergejkvyatkovskij@gmail.com>
2022-03-04 17:55:40 +0300

    corrections

--- DateToolsSwift/DateTools/Date+Components.swift
+++ DateToolsSwift/DateTools/Date+Components.swift
@@ -24,10 +24,12 @@ public extension Date {
      *
      */
     func component(_ component: Calendar.Component) -> Int {
-		let calendar = Calendar.autoupdatingCurrent
-		return calendar.component(component, from: self)
-	}
-	
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
+        return calendar.component(component, from: self)
+    }
+
     /**
      *  Convenient accessor of the date's `Calendar` components ordinality.
      *
@@ -38,10 +40,12 @@ public extension Date {
      *
      */
     func ordinality(of smaller: Calendar.Component, in larger: Calendar.Component) -> Int? {
-		let calendar = Calendar.autoupdatingCurrent
-		return calendar.ordinality(of: smaller, in: larger, for: self)
-	}
-	
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
+        return calendar.ordinality(of: smaller, in: larger, for: self)
+    }
+
     /**
      *  Use calendar components to determine how many units of a smaller component are inside 1 larger unit.
      *
@@ -54,17 +58,18 @@ public extension Date {
      *  - returns: The number of smaller units required to equal in 1 larger unit, given the date called on
      *
      */
-    @available(*, deprecated, message: "Calendar component hashes no longer yield relevant values and will always return nil. The function is deprecated and will be removed soon.")
     func unit(of smaller: Calendar.Component, in larger: Calendar.Component) -> Int? {
-		let calendar = Calendar.autoupdatingCurrent
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
         var units = 1
         var unitRange: Range<Int>?
         if larger.hashValue < smaller.hashValue {
             for x in larger.hashValue..<smaller.hashValue {
-                
+
                 var stepLarger: Calendar.Component
                 var stepSmaller: Calendar.Component
-                
+
                 switch(x) {
                 case 0:
                     stepLarger = Calendar.Component.era
@@ -111,7 +116,7 @@ public extension Date {
                 default:
                     return nil
                 }
-                
+
                 if unitRange?.count != nil {
                     units *= (unitRange?.count)!
                 }
@@ -119,213 +124,233 @@ public extension Date {
             return units
         }
         return nil
-	}
-	
-	// MARK: - Components
-	
+    }
+
+    // MARK: - Components
+
     /**
      *  Convenience getter for the date's `era` component
      */
     var era: Int {
-		return component(.era)
-	}
-	
+        return component(.era)
+    }
+
     /**
      *  Convenience getter for the date's `year` component
      */
     var year: Int {
-		return component(.year)
-	}
-	
+        return component(.year)
+    }
+
     /**
      *  Convenience getter for the date's `month` component
      */
     var month: Int {
-		return component(.month)
-	}
-	
+        return component(.month)
+    }
+
     /**
      *  Convenience getter for the date's `week` component
      */
     var week: Int {
-		return component(.weekday)
-	}
-	
+        return component(.weekday)
+    }
+
     /**
      *  Convenience getter for the date's `day` component
      */
     var day: Int {
-		return component(.day)
-	}
-	
+        return component(.day)
+    }
+
     /**
      *  Convenience getter for the date's `hour` component
      */
     var hour: Int {
-		return component(.hour)
-	}
-	
+        return component(.hour)
+    }
+
     /**
      *  Convenience getter for the date's `minute` component
      */
     var minute: Int {
-		return component(.minute)
-	}
-	
+        return component(.minute)
+    }
+
     /**
      *  Convenience getter for the date's `second` component
      */
     var second: Int {
-		return component(.second)
-	}
-	
+        return component(.second)
+    }
+
     /**
      *  Convenience getter for the date's `weekday` component
      */
     var weekday: Int {
-		return component(.weekday)
-	}
-	
+        return component(.weekday)
+    }
+
     /**
      *  Convenience getter for the date's `weekdayOrdinal` component
      */
     var weekdayOrdinal: Int {
-		return component(.weekdayOrdinal)
-	}
-	
+        return component(.weekdayOrdinal)
+    }
+
     /**
      *  Convenience getter for the date's `quarter` component
      */
     var quarter: Int {
-		return component(.quarter)
-	}
-	
+        return component(.quarter)
+    }
+
     /**
      *  Convenience getter for the date's `weekOfYear` component
      */
     var weekOfMonth: Int {
-		return component(.weekOfMonth)
-	}
-	
+        return component(.weekOfMonth)
+    }
+
     /**
      *  Convenience getter for the date's `weekOfYear` component
      */
     var weekOfYear: Int {
-		return component(.weekOfYear)
-	}
-	
+        return component(.weekOfYear)
+    }
+
     /**
      *  Convenience getter for the date's `yearForWeekOfYear` component
      */
     var yearForWeekOfYear: Int {
-		return component(.yearForWeekOfYear)
-	}
-    
+        return component(.yearForWeekOfYear)
+    }
+
     /**
      *  Convenience getter for the date's `daysInMonth` component
      */
     var daysInMonth: Int {
-        let calendar = Calendar.autoupdatingCurrent
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
         let days = calendar.range(of: .day, in: .month, for: self)
         return days!.count
     }
-    
+
     // MARK: - Set Components
-    
+
     /**
      *  Convenience setter for the date's `year` component
      */
     mutating func year(_ year: Int) {
         self = Date.init(year: year, month: self.month, day: self.day, hour: self.hour, minute: self.minute, second: self.second)
     }
-    
+
     /**
      *  Convenience setter for the date's `month` component
      */
     mutating func month(_ month: Int) {
         self = Date.init(year: self.year, month: month, day: self.day, hour: self.hour, minute: self.minute, second: self.second)
     }
-    
+
     /**
      *  Convenience setter for the date's `day` component
      */
     mutating func day(_ day: Int) {
         self = Date.init(year: self.year, month: self.month, day: day, hour: self.hour, minute: self.minute, second: self.second)
     }
-    
+
     /**
      *  Convenience setter for the date's `hour` component
      */
     mutating func hour(_ hour: Int) {
         self = Date.init(year: self.year, month: self.month, day: self.day, hour: hour, minute: self.minute, second: self.second)
     }
-    
+
     /**
      *  Convenience setter for the date's `minute` component
      */
     mutating func minute(_ minute: Int) {
         self = Date.init(year: self.year, month: self.month, day: self.day, hour: self.hour, minute: minute, second: self.second)
     }
-    
+
     /**
      *  Convenience setter for the date's `second` component
      */
     mutating func second(_ second: Int) {
         self = Date.init(year: self.year, month: self.month, day: self.day, hour: self.hour, minute: self.minute, second: second)
     }
-    
-    
+
     // MARK: - Bools
-	
+
     /**
      *  Determine if date is in a leap year
      */
     var isInLeapYear: Bool {
-		let yearComponent = component(.year)
-		
-		if yearComponent % 400 == 0 {
-			return true
-		}
-		if yearComponent % 100 == 0 {
-			return false
-		}
-		if yearComponent % 4 == 0 {
-			return true
-		}
-		return false
-	}
-	
+        let yearComponent = component(.year)
+
+        if yearComponent % 400 == 0 {
+            return true
+        }
+        if yearComponent % 100 == 0 {
+            return false
+        }
+        if yearComponent % 4 == 0 {
+            return true
+        }
+        return false
+    }
+
     /**
      *  Determine if date is within the current day
      */
     var isToday: Bool {
-		let calendar = Calendar.autoupdatingCurrent
-		return calendar.isDateInToday(self)
-	}
-	
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
+        return calendar.isDateInToday(self)
+    }
+
     /**
      *  Determine if date is within the day tomorrow
      */
     var isTomorrow: Bool {
-		let calendar = Calendar.autoupdatingCurrent
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
         return calendar.isDateInTomorrow(self)
-	}
-	
+    }
+
     /**
      *  Determine if date is within yesterday
      */
     var isYesterday: Bool {
-		let calendar = Calendar.autoupdatingCurrent
+        var calendar = Calendar.autoupdatingCurrent
+        calendar.timeZone = TimeZone.current
+        calendar.locale = Locale(identifier: "en_US_POSIX")
         return calendar.isDateInYesterday(self)
-	}
-	
+    }
+
     /**
      *  Determine if date is in a weekend
      */
     var isWeekend: Bool {
-		if weekday == 7 || weekday == 1 {
-			return true
-		}
-		return false
-	}
+        if weekday == 7 || weekday == 1 {
+            return true
+        }
+        return false
+    }
+
+    /**
+     *  First day of the month
+     */
+    var firstDayOfTheMonth: Date? {
+        return Calendar.current.dateComponents([.calendar, .year,.month], from: self).date
+    }
+
+    var lastDayOfMonth: Date? {
+        guard let date = self.firstDayOfTheMonth else { return nil }
+        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: date)
+    }
+
 }
